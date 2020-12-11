a31b8b8ce7696763052b7eff7a78b675036a6c7b
Alessandro Bettarini <abettarini@icloud.com>
2020-12-11 17:59:45 +0100

    Issue #82 refactor print code

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -40,7 +40,6 @@
 #include "DataStore.hpp"
 #include "InteractionsHtmlView.hpp"
 #include "PrescriptionItem.hpp"
-#include "PrescriptionsCart.hpp"
 #include "ItemCellView.hpp"
 #include "config.h"
 #include "SignatureView.hpp"
@@ -56,12 +55,6 @@
 
 // Alternatively implement its own tabview to show the results
 #define CSV_EXPORT_RESTORES_PREVIOUS_STATE
-
-// Global print data, to remember settings during the session
-wxPrintData *g_printData = NULL;
-
-// Global page setup data
-wxPageSetupDialogData* g_pageSetupData = NULL;
 
 /// 84
 // Database types
@@ -92,23 +85,8 @@ static int mCurrentSearchState = kss_Title;
 static int mCurrentWebView = kExpertInfoView;
 static wxString mCurrentSearchKey;
 
-static int myTextWrapper(wxString &s, const int newlinePos)
-{
-    //const int newlinePos = 60;
-    int n = s.length();
-    int countNewline = 0;
-    for (int i = newlinePos; i<n; i+=newlinePos) {
-        s.insert(i, "\n");
-        countNewline++;
-        //yPos += 10;
-    }
-
-    return countNewline;
-}
-
-// 212
-#define NUM_ACTIVE_PRESCRIPTIONS   3
-static PrescriptionsCart mPrescriptionsCart[NUM_ACTIVE_PRESCRIPTIONS];
+// Class variable
+PrescriptionsCart MainWindow::mPrescriptionsCart[];
 
 // Events not processed by MainWindow will, by default, be handled by MainWindowBase
 BEGIN_EVENT_TABLE(MainWindow, MainWindowBase)
@@ -323,16 +301,7 @@ MainWindow::MainWindow( wxWindow* parent )
     Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainWindow::OnQuit));
 #endif
     
-    g_printData = new wxPrintData;
-    g_printData->SetPaperId(wxPAPER_A4);    // for everyone else
-
-    g_pageSetupData = new wxPageSetupDialogData;
-    // copy over initial paper size from print record
-    (*g_pageSetupData) = *g_printData;
-
-    // Set some initial page margins in mm.
-    g_pageSetupData->SetMarginTopLeft(wxPoint(15, 15));
-    g_pageSetupData->SetMarginBottomRight(wxPoint(15, 15));
+    initPrint();
 }
 
 MainWindow::~MainWindow()
@@ -345,8 +314,7 @@ MainWindow::~MainWindow()
 #endif
 #endif
     
-    delete g_printData;
-    delete g_pageSetupData;
+    terminatePrint();
 }
 
 #ifndef __APPLE_
@@ -710,117 +678,6 @@ void MainWindow::printTechInfo()
     //bool previewResult = m_Prn->PreviewText( pageSource); // it returns immediately
     m_Prn->PrintText( pageSource);
     wxDELETE(m_Prn);
-}
-
-// 1071
-// Reference wxWidgets sample printing.app printing.cpp:384
-void MainWindow::printPrescription()
-{
-    std::clog << __PRETTY_FUNCTION__ << std::endl;
-    wxPrintDialogData printDialogData(* g_printData);
-    wxPrintPreview *preview =
-    new wxPrintPreview(new MyPrintout(this), new MyPrintout(this), &printDialogData);
-    if (!preview->IsOk())
-    {
-        delete preview;
-        wxLogError("There was a problem previewing.\nPerhaps your current printer is not set correctly?");
-        return;
-    }
-
-    wxPreviewFrame *frame =
-        new wxPreviewFrame(preview, this, "Print Preview", wxPoint(100, 100), wxSize(600, 650));
-    frame->Centre(wxBOTH);
-    frame->InitializeWithModality(wxPreviewFrame_AppModal);
-    frame->Show();
-}
-
-void MainWindow::Draw(wxDC&dc)
-{
-#ifndef NDEBUG
-    std::clog << __PRETTY_FUNCTION__ << " TODO:" << std::endl;
-#endif
-
-    const wxCoord xPos = 10;
-    const wxCoord xOffset = 127;
-    wxCoord yPos = 30;
-    const wxCoord lineHeight = 9;
-    const wxCoord wrapAt = 65;
-
-    dc.SetBackground(*wxWHITE_BRUSH);
-    // dc.Clear();
-    wxFont m_testFont = wxFontInfo(6).Family(wxFONTFAMILY_DEFAULT);
-    //std::clog << "font size: " << m_testFont.GetFractionalPointSize() << std::endl;
-    dc.SetFont(m_testFont);
-
-    // dc.SetBackgroundMode(wxBRUSHSTYLE_TRANSPARENT);
-
-    int row = mySectionTitles->GetSelectedRow();
-    dc.DrawText(mListOfSectionTitles[row], xPos, yPos); // "RZ_2020-12-10T153736"
-    dc.DrawText("Page 1 of 1", xPos+xOffset, yPos);
-    yPos += 2*lineHeight;
-
-    dc.DrawText(myPatientAddressTextField->GetValue(), xPos, yPos);
-    dc.DrawText(myOperatorIDTextField->GetValue(), xPos+xOffset, yPos);
-    yPos += 5*lineHeight;
-
-    wxSize sz(90,40);
-    wxImage img = mySignView->getSignaturePNG();
-    //img.Resize(sz, wxPoint(0,0)); // add border or crop
-    //img = img.Scale(sz.x, sz.y, wxIMAGE_QUALITY_HIGH);
-    img.Rescale(sz.x, sz.y, wxIMAGE_QUALITY_HIGH);
-    wxBitmap m_bitmap = wxBitmap(img);
-    if (m_bitmap.IsOk())
-        dc.DrawBitmap( m_bitmap, xPos+xOffset, yPos );
-#if 1
-    sz.IncBy(3);
-    dc.SetBrush(*wxTRANSPARENT_BRUSH);
-    dc.SetPen(*wxBLACK_PEN);
-    dc.DrawRectangle(xPos+xOffset, yPos, sz.x, sz.y);
-#endif
-
-    dc.DrawText(myPlaceDateField->GetLabelText(), xPos, yPos);
-
-    yPos += 5*lineHeight;
-
-    dc.SetPen(*wxBLACK_PEN);
-    dc.SetBrush(*wxLIGHT_GREY_BRUSH);
-    dc.DrawRectangle(0, yPos, 230, 350-yPos);
-    dc.SetBrush(*wxTRANSPARENT_BRUSH);
-    yPos += lineHeight;
-
-    wxTreeItemId rootItem = myPrescriptionsTableView->GetRootItem();
-    if (!rootItem.IsOk())
-        return;
-
-    int numRows = myPrescriptionsTableView->GetChildrenCount(rootItem, false);
-    std::clog << "numRows: " << numRows << std::endl;
-
-    const int cartNo = 0;
-    int cartSize = mPrescriptionsCart[ cartNo].cart.size();
-    std::clog << "cartSize: " << cartSize << std::endl;
-
-    for (int i=0; i<cartSize; i++) {
-        PrescriptionItem *item = mPrescriptionsCart[ cartNo].getItemAtIndex(i);
-        wxString str;
-
-        str.Printf("%s", item->fullPackageInfo);
-        int n = myTextWrapper(str, wrapAt);
-        dc.DrawText(str, xPos, yPos);
-        yPos += lineHeight*(1+n);
-
-//        str.Printf( "%s", item->eanCode );
-//        dc.DrawText(str, xPos, yPos);
-//        yPos += lineHeight;
-
-        if (item->comment.length() > 0) {
-            str.Printf("%s", item->comment);
-            int n = myTextWrapper(str, wrapAt);
-            dc.DrawText(str, xPos, yPos);
-            yPos += lineHeight*(1+n);
-        }
-
-        yPos += lineHeight;
-    }
 }
 
 // 1249
